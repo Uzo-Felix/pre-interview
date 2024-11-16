7389539557c070138db88ae1cc0352a2700465d8
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2024-11-16 11:29:58 +0300

    refactor: update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from './contexts/ThemeContext';
+import ArticleList from './components/ArticleList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <CssBaseline />
+      <ArticleList />
+    </ThemeProvider>
   );
 }
 
