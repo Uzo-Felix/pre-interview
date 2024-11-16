2bf92919db253c50c9a978af9ff587a33cbc3193
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2024-11-16 13:23:59 +0300

    refactor: modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { CssBaseline } from '@mui/material';
+import React from 'react';
 import { ThemeProvider } from './contexts/ThemeContext';
-import ArticleList from './components/ArticleList';
+import { ThemeToggle } from './components/ThemeToggle';
+import { ArticleList } from './components/ArticleList';
 
 function App() {
   return (
     <ThemeProvider>
-      <CssBaseline />
-      <ArticleList />
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+        <header className="p-4 border-b dark:border-gray-700">
+          <div className="container mx-auto flex justify-between items-center">
+            <h1 className="text-2xl font-bold">Article App</h1>
+            <ThemeToggle />
+          </div>
+        </header>
+        <main>
+          <ArticleList />
+        </main>
+      </div>
     </ThemeProvider>
   );
 }
